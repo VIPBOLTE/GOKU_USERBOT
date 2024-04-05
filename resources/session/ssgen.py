70867f1b01197c470e16df0bc6034b0d70d0cd8f
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-06 00:21:21 +0530

    Update ssgen.py

--- resources/session/ssgen.py
+++ resources/session/ssgen.py
@@ -1,21 +1,9 @@
-#!/usr/bin/python3
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 import os
 from time import sleep
 
-ULTROID = r"""
-  _    _ _ _             _     _
- | |  | | | |           (_)   | |
- | |  | | | |_ _ __ ___  _  __| |
- | |  | | | __| '__/ _ \| |/ _  |
- | |__| | | |_| | | (_) | | (_| |
-  \____/|_|\__|_|  \___/|_|\__,_|
+GOKU_USERBOT= r"""
+  MR GOKU ❤ PROGRAMMING
 """
 
 
@@ -63,7 +51,7 @@ def telethon_session():
 
         x = "\bDone. Installed and imported Telethon."
     clear_screen()
-    print(ULTROID)
+    print(GOKU_USERBOT)
     print(x)
 
     # the imports
@@ -80,12 +68,12 @@ def telethon_session():
 
     # logging in
     try:
-        with TelegramClient(StringSession(), API_ID, API_HASH) as ultroid:
-            print("Generating a string session for •ULTROID•")
+        with TelegramClient(StringSession(), API_ID, API_HASH) as GOKU_USERBOT:
+            print("Generating a string session for •GOKU_USERBOT•")
             try:
-                ultroid.send_message(
+                GOKU_USERBOT.send_message(
                     "me",
-                    f"**ULTROID** `SESSION`:\n\n`{ultroid.session.save()}`\n\n**Do not share this anywhere!**",
+                    f"**GOKU_USERBOT** `SESSION`:\n\n`{ultroid.session.save()}`\n\n**Do not share this anywhere!**",
                 )
                 print(
                     "Your SESSION has been generated. Check your Telegram saved messages!"
@@ -93,7 +81,7 @@ def telethon_session():
                 return
             except UserIsBotError:
                 print("You are trying to Generate Session for your Bot's Account?")
-                print("Here is That!\n{ultroid.session.save()}\n\n")
+                print("Here is That!\n{GOKU_USERBOT.session.save()}\n\n")
                 print("NOTE: You can't use that as User Session..")
     except ApiIdInvalidError:
         print(
@@ -109,7 +97,7 @@ def telethon_session():
     except Exception as er:
         print("Unexpected Error Occurred while Creating Session")
         print(er)
-        print("If you think It as a Bug, Report to @UltroidSupportChat.\n\n")
+        print("If you think It as a Bug, Report to @goku_groupz.\n\n")
 
 
 def pyro_session():
@@ -125,7 +113,7 @@ def pyro_session():
         from pyrogram import Client
         
     clear_screen()
-    print(ULTROID)
+    print(GOKU_USERBOT)
     print(x)
 
     # generate a session
@@ -136,7 +124,7 @@ def pyro_session():
             ss = pyro.export_session_string()
             pyro.send_message(
                 "me",
-                f"`{ss}`\n\nAbove is your Pyrogram Session String for @TheUltroid. **DO NOT SHARE it.**",
+                f"`{ss}`\n\nAbove is your Pyrogram Session String for @channelz_k. **DO NOT SHARE it.**",
             )
             print("Session has been sent to your saved messages!")
             exit(0)
@@ -147,11 +135,11 @@ def pyro_session():
 
 def main():
     clear_screen()
-    print(ULTROID)
+    print(GOKU_USERBOT)
     try:
         type_of_ss = int(
             input(
-                "\nUltroid supports both telethon as well as pyrogram sessions.\n\nWhich session do you want to generate?\n1. Telethon Session.\n2. Pyrogram Session.\n\nEnter choice:  "
+                "\nGOKU_USERBOT supports both telethon as well as pyrogram sessions.\n\nWhich session do you want to generate?\n1. Telethon Session.\n2. Pyrogram Session.\n\nEnter choice:  "
             )
         )
     except Exception as e:
