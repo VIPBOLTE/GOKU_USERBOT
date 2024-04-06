46617029b75d76ea9b852f4b86dc267a19f1f06b
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-06 19:34:47 +0530

    Update asstcmd.py

--- plugins/asstcmd.py
+++ plugins/asstcmd.py
@@ -1,18 +1,11 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 from . import get_help
 
 __doc__ = get_help("help_asstcmd")
 
 import os
 
-from pyUltroid.dB.asstcmd_db import add_cmd, cmd_reply, list_cmds, rem_cmd
-from pyUltroid.fns.tools import create_tl_btn, format_btn, get_msg_button
+from GOKU_USER.dB.asstcmd_db import add_cmd, cmd_reply, list_cmds, rem_cmd
+from GOKU_USER.fns.tools import create_tl_btn, format_btn, get_msg_button
 
 try:
     from telegraph import upload_file as uf
@@ -20,10 +13,10 @@ except ImportError:
     uf = None
 from telethon import events, utils
 
-from . import asst, get_string, mediainfo, udB, ultroid_cmd
+from . import asst, get_string, mediainfo, udB, GOKU_USERBOT_cmd
 
 
-@ultroid_cmd(pattern="addcmd( (.*)|$)")
+@GOKU_USERBOT_cmd(pattern="addcmd( (.*)|$)")
 async def ac(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     wt = await e.get_reply_message()
