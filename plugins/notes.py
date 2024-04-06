b756562ac3563c0ffcdc4d9eb792e2c99b3347ed
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-06 10:57:42 +0530

    Update notes.py

--- plugins/notes.py
+++ plugins/notes.py
@@ -1,9 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 """
 ✘ Commands Available -
 
@@ -25,14 +19,14 @@ import os
 from telegraph import upload_file as uf
 from telethon.utils import pack_bot_file_id
 
-from pyUltroid.dB.notes_db import add_note, get_notes, list_note, rem_note
-from pyUltroid.fns.tools import create_tl_btn, format_btn, get_msg_button
+from GOKU_USER.dB.notes_db import add_note, get_notes, list_note, rem_note
+from GOKU_USER.fns.tools import create_tl_btn, format_btn, get_msg_button
 
-from . import events, get_string, mediainfo, udB, ultroid_bot, ultroid_cmd
+from . import events, get_string, mediainfo, udB, GOKU_USERBOT_bot, GOKU_USERBOT_cmd
 from ._inline import something
 
 
-@ultroid_cmd(pattern="addnote( (.*)|$)", admins_only=True)
+@GOKU_USERBOT_cmd(pattern="addnote( (.*)|$)", admins_only=True)
 async def an(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     wt = await e.get_reply_message()
@@ -71,10 +65,10 @@ async def an(e):
             txt, btn = get_msg_button(wt.text)
         add_note(chat, wrd, txt, None, btn)
     await e.eor(get_string("notes_2").format(wrd))
-    ultroid_bot.add_handler(notes, events.NewMessage())
+    GOKU_USERBOT_bot.add_handler(notes, events.NewMessage())
 
 
-@ultroid_cmd(pattern="remnote( (.*)|$)", admins_only=True)
+@GOKU_USERBOT_cmd(pattern="remnote( (.*)|$)", admins_only=True)
 async def rn(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     chat = e.chat_id
@@ -86,7 +80,7 @@ async def rn(e):
     await e.eor(f"Done Note: `#{wrd}` Removed.")
 
 
-@ultroid_cmd(pattern="listnote$", admins_only=True)
+@GOKU_USERBOT_cmd(pattern="listnote$", admins_only=True)
 async def lsnote(e):
     if x := list_note(e.chat_id):
         sd = "Notes Found In This Chats Are\n\n"
@@ -109,4 +103,4 @@ async def notes(e):
 
 
 if udB.get_key("NOTE"):
-    ultroid_bot.add_handler(notes, events.NewMessage())
+    GOKU_USERBOT_bot.add_handler(notes, events.NewMessage())
