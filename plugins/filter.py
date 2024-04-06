2381b01fa6d46edbf2dfb02ee0adea0795dd3cfe
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-06 12:22:08 +0530

    Update filter.py

--- plugins/filter.py
+++ plugins/filter.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 from . import get_help
 
 __doc__ = get_help("help_filter")
@@ -16,14 +9,14 @@ from telegraph import upload_file as uf
 from telethon.tl.types import User
 from telethon.utils import pack_bot_file_id
 
-from pyUltroid.dB.filter_db import add_filter, get_filter, list_filter, rem_filter
-from pyUltroid.fns.tools import create_tl_btn, format_btn, get_msg_button
+from GOKU_USER.dB.filter_db import add_filter, get_filter, list_filter, rem_filter
+from GOKU_USER.fns.tools import create_tl_btn, format_btn, get_msg_button
 
-from . import events, get_string, mediainfo, udB, ultroid_bot, ultroid_cmd
+from . import events, get_string, mediainfo, udB, GOKU_USERBOT_bot, GOKU_USERBOT_cmd
 from ._inline import something
 
 
-@ultroid_cmd(pattern="addfilter( (.*)|$)")
+@GOKU_USERBOT_cmd(pattern="addfilter( (.*)|$)")
 async def af(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     wt = await e.get_reply_message()
@@ -59,10 +52,10 @@ async def af(e):
             txt, btn = get_msg_button(wt.text)
         add_filter(chat, wrd, txt, None, btn)
     await e.eor(get_string("flr_4").format(wrd))
-    ultroid_bot.add_handler(filter_func, events.NewMessage())
+    GOKU_USERBOT_bot.add_handler(filter_func, events.NewMessage())
 
 
-@ultroid_cmd(pattern="remfilter( (.*)|$)")
+@GOKU_USERBOT_cmd(pattern="remfilter( (.*)|$)")
 async def rf(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     chat = e.chat_id
@@ -72,7 +65,7 @@ async def rf(e):
     await e.eor(get_string("flr_5").format(wrd))
 
 
-@ultroid_cmd(pattern="listfilter$")
+@GOKU_USERBOT_cmd(pattern="listfilter$")
 async def lsnote(e):
     if x := list_filter(e.chat_id):
         sd = "Filters Found In This Chats Are\n\n"
@@ -99,4 +92,4 @@ async def filter_func(e):
 
 
 if udB.get_key("FILTERS"):
-    ultroid_bot.add_handler(filter_func, events.NewMessage())
+    GOKU_USERBOT_bot.add_handler(filter_func, events.NewMessage())
