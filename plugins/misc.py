04fe9e0312654314deaff37cedf1fc306e430201
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-06 11:23:25 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,8 +1,3 @@
-# Ultroid - UserBot
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 """
 ✘ Commands Available -
 
@@ -36,10 +31,10 @@ try:
 except ImportError:
     Img2HTMLConverter = None
 
-from . import async_searcher, get_random_user_data, get_string, re, ultroid_cmd
+from . import async_searcher, get_random_user_data, get_string, re, GOKU_USERBOT_cmd
 
 
-@ultroid_cmd(pattern="eod$")
+@GOKU_USERBOT_cmd(pattern="eod$")
 async def diela(e):
     m = await e.eor(get_string("com_1"))
     li = "https://daysoftheyear.com"
@@ -55,7 +50,7 @@ async def diela(e):
     await m.edit(te, link_preview=False)
 
 
-@ultroid_cmd(
+@GOKU_USERBOT_cmd(
     pattern="pntrst( (.*)|$)",
 )
 async def pinterest(e):
@@ -76,7 +71,7 @@ async def pinterest(e):
     await e.client.send_file(e.chat_id, file, caption=f"Pin:- {m}")
 
 
-@ultroid_cmd(pattern="gadget( (.*)|$)")
+@GOKU_USERBOT_cmd(pattern="gadget( (.*)|$)")
 async def mobs(e):
     mat = e.pattern_match.group(1).strip()
     if not mat:
@@ -111,7 +106,7 @@ async def mobs(e):
     await bt.delete()
 
 
-@ultroid_cmd(pattern="randomuser")
+@GOKU_USERBOT_cmd(pattern="randomuser")
 async def _gen_data(event):
     x = await event.eor(get_string("com_1"))
     msg, pic = await get_random_user_data()
@@ -119,7 +114,7 @@ async def _gen_data(event):
     await x.delete()
 
 
-@ultroid_cmd(
+@GOKU_USERBOT_cmd(
     pattern="ascii( (.*)|$)",
 )
 async def _(e):
