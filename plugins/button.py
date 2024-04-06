c4619a2148569508ab2cdafcfefa5f5aab46c9ad
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-06 13:31:10 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 from . import get_help
 
 __doc__ = get_help("help_button")
