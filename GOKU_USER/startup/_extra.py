2c19d105e52a4a079787d8df32ea8672420dd587
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-07 12:08:35 +0530

    Update _extra.py

--- GOKU_USER/startup/_extra.py
+++ GOKU_USER/startup/_extra.py
@@ -1,15 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
-
-# https://bugs.python.org/issue26789
-# 'open' not defined has been fixed in Python3.10
-# for other older versions, something need to be done.
-
-
 def _fix_logging(handler):
     handler._builtin_open = open
 
