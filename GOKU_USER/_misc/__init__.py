e256e37c8ff9005d1c3d3c238eda18691c801f83
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-07 16:31:05 +0530

    Update __init__.py

--- GOKU_USER/_misc/__init__.py
+++ GOKU_USER/_misc/__init__.py
@@ -1,11 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
-
-
 from .. import *
 
 CMD_HELP = {}
