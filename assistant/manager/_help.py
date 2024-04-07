912cc09eef5782afed25176824273fe73639c3db
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-07 11:00:38 +0530

    Update _help.py

--- assistant/manager/_help.py
+++ assistant/manager/_help.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 from . import *
 
 START = """
@@ -14,7 +7,7 @@ START = """
 ✘  /help : Get This Message.
 ✘  /repo : Get Bot's Repo..
 
-🧑‍💻 Join **@TeamUltroid**
+🧑‍💻 Join **@channelz_k**
 """
 
 ADMINTOOLS = """✘ **AdminTools** ✘
